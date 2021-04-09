ef1c930c2f674565b4c6263be3a526cc2b0bfe8a
GuenterHummel <guenter.hummel@mac.com>
2021-04-09 20:04:06 +0200

    project update

--- firstcl/src/firstcl/core.clj
+++ firstcl/src/firstcl/core.clj
@@ -1,16 +1,6 @@
 (ns firstcl.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!")
-  (println "Super Stuff, das Clojure")
-  (.println (System/out) "Hi")
-  (println ("LOGIN OK <" "Siva" "> :" (check-login "siva" "secrets"))))
-
 (def users {"kyle" {:password "secretk" :number-pets 2}
             "siva" {:password "secrets" :number-pets 4}
             "rob" {:password "secretr" :number-pets 6}
@@ -19,3 +9,14 @@
 (defn check-login [username password]
   (let [actual-password ((users username) :password)]
     (= actual-password password)))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  ;; work around dangerous default behaviour in Clojure
+  (alter-var-root #'*read-eval* (constantly false))
+  (println "Hello, World!")
+  (println "Super Stuff, das Clojure")
+  (.println (System/out) "Hi")
+  (println "LOGIN OK <" "Siva" "> :" (check-login "siva" "secrets")))
+
